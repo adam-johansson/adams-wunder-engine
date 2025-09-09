aadfc1cf4637d83d5b5082b840bf97a4150bb354
adam-johansson <adam.l.johansson@chalmers.se>
2025-09-09 13:29:48 +0200

    Added constant temperature

--- thermo/validation_codes/nox_pressure_cantera_kinetics_test.py
+++ thermo/validation_codes/nox_pressure_cantera_kinetics_test.py
@@ -14,13 +14,21 @@ xi_N2_0, xi_O2_0, xi_CO2_0, xi_H2O_0, xi_Ar_0, _ = molar_fractions(
 )
 
 
-T = 2780
-p = 1e5
+T = 2800
+p = 50e5
 
 gas = ct.Solution("gri30.yaml")
 gas.TPX = T, p, f'CO2:{xi_CO2_0},O2:{xi_O2_0},N2:{xi_N2_0},Ar:{xi_Ar_0}'
 r = ct.IdealGasConstPressureReactor(gas)
 
+# Create a thermal reservoir to maintain temperature
+env = ct.Reservoir(gas)  # Large thermal reservoir
+
+# Connect with a heat transfer wall
+wall = ct.Wall(r, env)
+wall.heat_transfer_coeff = 1e12  # Large heat transfer coefficient
+
+# Add to reactor network
 sim = ct.ReactorNet([r])
 sim.verbose = True
 
@@ -29,7 +37,7 @@ delta_T_max = 20.
 r.set_advance_limit('temperature', delta_T_max)
 
 dt_max = 1.e-5
-t_end = 5e4 * dt_max
+t_end = 1e4 * dt_max
 states = ct.SolutionArray(gas, extra=['t'])
 
 print('{:10s} {:10s} {:10s} {:14s}'.format(
