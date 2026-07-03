a32811990431e3e815016ef4a010eb19da919cde
Adam Johansson <adam.l.johansson@chalmers.se>
2026-07-03 15:05:53 +0200

    sista innan gamla datorn

--- piston_engine/input/EGR_validation/4T50ME.py
+++ piston_engine/input/EGR_validation/4T50ME.py
@@ -27,7 +27,7 @@ lms = s/(2*l_con)  #connecting rod ratio
 # inlet and outlet conditions
 p_in = 3.65e5  # inlet pressure (kaiser had 8 bar cruise 26 bar take off)
 T_in = 390  # inlet temperature (670 cruise 770 TO)
-p_ratio = 0.95  # pressure ratio after and before engine
+p_ratio = 0.90  # pressure ratio after and before engine
 
 mf_tot = 0.034
 
