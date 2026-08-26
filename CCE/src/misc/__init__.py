b386d7c08bac637da17bbd8fcd77a87a32bb2ba4
Adam Johansson <adam.l.johansson@chalmers.se>
2026-08-26 17:06:41 +0200

    removed matchpower_NN from __init__ in misc

--- CCE/src/misc/__init__.py
+++ CCE/src/misc/__init__.py
@@ -15,11 +15,11 @@ from .output import (
 from .postprocess import energy_flow_fuel, power_balance
 from .calc_efficiencies import calc_efficiencies_cce, calc_efficiencies_recuperated_h2_geared, calc_efficiencies_jetA_geared
 from .fpr_opt import fpr_opt
-from .match_power_NN import match_power_nn
+#from .match_power_NN import match_power_nn
 from .match_power_specific import match_power_specific
 from .match_power_bore import match_power_bore
 from .match_power_lifehack import match_power_lifehack
 from .calc_entropy_array import entropy_array
-from .EINOx_equiv_temp import equivalent_temperature
+#from .EINOx_equiv_temp import equivalent_temperature
 from .trade_factors import thrust_requirement_function, load_trade_factors, get_block_energy
 from .weight import calculate_powerplant_weight
