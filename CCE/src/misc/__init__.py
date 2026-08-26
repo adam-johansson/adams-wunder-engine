c89ac8953215307797911c01d1f6c2a035bd8d8f
Adam Johansson <adam.l.johansson@chalmers.se>
2026-08-26 17:07:39 +0200

    same

--- CCE/src/misc/__init__.py
+++ CCE/src/misc/__init__.py
@@ -16,7 +16,7 @@ from .postprocess import energy_flow_fuel, power_balance
 from .calc_efficiencies import calc_efficiencies_cce, calc_efficiencies_recuperated_h2_geared, calc_efficiencies_jetA_geared
 from .fpr_opt import fpr_opt
 #from .match_power_NN import match_power_nn
-from .match_power_specific import match_power_specific
+#from .match_power_specific import match_power_specific
 from .match_power_bore import match_power_bore
 from .match_power_lifehack import match_power_lifehack
 from .calc_entropy_array import entropy_array
